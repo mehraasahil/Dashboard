6b7946cc50402194030fe91b21d6c4b931d528dd
mehraasahil <mehra3249@gmail.com>
2023-08-09 14:05:09 +0530

    added

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const User = require('./db/User');
+
+require ('./db/config')
+const app = express()
+
+app.use(express.json())
+
+app.post('/register',async(req,resp)=>{
+  const data = new User(req.body);
+  const result = await data.save();
+  resp.send(result)
+ console.log(req.body)
+})
+
+ app.listen(4000)
